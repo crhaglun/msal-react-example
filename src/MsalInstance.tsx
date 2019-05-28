ac8668ea72c45065572911382ca5f1d9d8676761
Christoffer Haglund <crhaglun@microsoft.com>
2019-05-28 15:38:16 -0700

    Adding better comments

--- src/MsalInstance.tsx
+++ src/MsalInstance.tsx
@@ -1,28 +1,34 @@
 import { UserAgentApplication, AuthError, AuthResponse } from "msal";
 
 declare global {
-    interface Window { msalInstance: UserAgentApplication }
+    interface Window { userAgentApplication: UserAgentApplication }
 }
 
 export const createUserAgentApplication = () => {
 
-    if (!window.msalInstance) {
+    if (!window.userAgentApplication) {
         const msalConfig = {
             auth: {
+                // Application registration ID
                 clientId: 'c454a5cb-2667-4927-820a-89a1e25f0f8d',
+
+                // Authority endpoint
+                // Azure AD + consumer MSA: https://login.microsoftonline.com/common
+                // Azure AD only:           https://login.microsoftonline.com/organizations
+                // Specific AAD tenant:     https://login.microsoftonline.com/<tenant ID>  
                 authority: "https://login.microsoftonline.com/common",
             }
         }
 
-        window.msalInstance = new UserAgentApplication(msalConfig)
+        window.userAgentApplication = new UserAgentApplication(msalConfig)
 
         const redirectCallback = (authErr: AuthError, response?: AuthResponse) => {
             console.log(authErr);
             console.log(response);
         }
 
-        window.msalInstance.handleRedirectCallback(redirectCallback)
+        window.userAgentApplication.handleRedirectCallback(redirectCallback)
     }
 
-    return window.msalInstance
+    return window.userAgentApplication
 }
--- src/useAuthenticationStateHook.tsx
+++ src/useAuthenticationStateHook.tsx
@@ -3,22 +3,24 @@ import React from "react";
 
 interface AuthenticationState {
     readonly description: string
+    readonly requestAccessToken: () => void
     readonly authResponse?: AuthResponse | AuthError
     readonly authHeaders?: Headers
-    readonly requestAccessToken: () => void
 }
 
 export function useAuthenticationState(userAgentApplication: UserAgentApplication, description: string, scopes: string[]): AuthenticationState {
 
-    const request = {
-        scopes: scopes
-    }
+    const request = { scopes }
 
     const requestAccessToken = () => {
         try {
+            // Use redirect to aquire a new access token; simplifies our app state
+            // because we don't need to keep track of pending actions
             userAgentApplication.acquireTokenRedirect(request);
         } catch (err) {
             if (err.errorCode === "user_login_error") {
+                // If we fail because the user is not signed in, redirect to the login
+                // flow instead
                 userAgentApplication.loginRedirect(request);
             } else {
                 setAuthState((currentState: AuthenticationState) => { return { ...currentState, authResponse: err } })
@@ -26,10 +28,10 @@ export function useAuthenticationState(userAgentApplication: UserAgentApplicatio
         }
     }
 
-    const [authState, setAuthState] = React.useState<AuthenticationState>(
-        { description, requestAccessToken });
+    const [authState, setAuthState] = React.useState<AuthenticationState>({ description, requestAccessToken });
 
     React.useEffect(() => {
+        // Try to retrieve the currently cached access token and prepare a Headers object
         if (!authState.authResponse) {
             const initializeAuthResponse = async () => {
                 try {
